731caf7886bcb51c57e165c5bacf272afe87f4cc
Cody <meanderingmonarch@gmail.com>
2016-02-17 07:47:58 -0600

    Changed post data to objects and made it viewable via angular

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ var app = angular.module('meannit', []);
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
-		$scope.test = 'Hello World!';
+		$scope.test = 'Hello World! Woot';
+		$scope.posts = [
+		{title: 'post1', upvotes: 5},
+		{title: 'post2', upvotes: 1},
+		{title: 'post3', upvotes: 12},
+		{title: 'post4', upvotes: 76},]
 	}]);
