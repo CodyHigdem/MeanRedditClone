698ddf02579cdda99b376da44b8b381fc5cdd75c
Cody <meanderingmonarch@gmail.com>
2016-02-17 08:05:32 -0600

    Feature: added title and links to angular js

--- app.js
+++ app.js
@@ -8,5 +8,18 @@ app.controller('MainCtrl', [
 		{title: 'post1', upvotes: 5},
 		{title: 'post2', upvotes: 1},
 		{title: 'post3', upvotes: 12},
-		{title: 'post4', upvotes: 76},]
+		{title: 'post4', upvotes: 76},];
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === '') { return; }
+		  	$scope.posts.push({
+		  		title: $scope.title, 
+		  		link: $scope.link,
+		  		upvotes: 0});
+		  	$scope.title = '';
+		  	$scope.link = '';
+		};
+
+		$scope.increaseUpvotes = function(post){
+			post.upvotes += 1;
+		}
 	}]);
