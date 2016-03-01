9d49fedafd3f577b27837da5fab89535be533be1
Cody <meanderingmonarch@gmail.com>
2016-03-01 10:40:07 -0600

    Testing Routes

--- meannit/routes/index.js
+++ meannit/routes/index.js
@@ -10,15 +10,12 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-//GET ROUTE
-router.get('/posts', function(req, res, next){
-	Post.find(function(err, posts){
-		if(err){
-			next(err); 
-		}
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
 
-		res.json(posts);
-	})
+    res.json(posts);
+  });
 });
 
 router.post('/posts', function(req, res, next) {
